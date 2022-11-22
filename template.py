c00b170ff1cfa8be851c92205497526155aa0572
Hae-K <bhk4392@inha.edu>
2022-11-22 10:10:36 +0900

    OSS_template.py

---
+++ template.py
@@ -0,0 +1,117 @@
+# PLEASE WRITE THE GITHUB URL BELOW!
+#
+
+import sys
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
+
+
+def load_dataset(dataset_path):
+    # To-Do: Implement this function
+    data = pd.read_csv(dataset_path, header=0, index_col=0)
+    return data
+
+
+def dataset_stat(dataset_df):
+    # To-Do: Implement this function
+    features = dataset_df.shape[1]
+    class_0 = dataset_df.groupby("target").size()[0]
+    class_1 = dataset_df.groupby("target").size()[1]
+    return features, class_0, class_1
+
+
+def split_dataset(dataset_df, testset_size):
+    # To-Do: Implement this function
+    X = dataset_df.drop(columns="target", axis=1)
+    y = dataset_df["target"]
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X, y, test_size=testset_size, random_state=2)
+    return X_train, X_test, Y_train, Y_test
+
+
+def decision_tree_train_test(x_train, x_test, y_train, y_test):
+    # To-Do: Implement this function
+    df_cls = DecisionTreeClassifier()
+    df_cls.fit(x_train, y_train)
+    Acc = accuracy_score(y_test, df_cls.predict(x_test))
+    mat = confusion_matrix(y_test, df_cls.predict(x_test))
+    tp = mat[0][0]
+    fp = mat[0][1]
+    fn = mat[1][0]
+    Prec = tp/(tp+fp)
+    Rec = tp/(tp+fn)
+    return Acc, Prec, Rec
+
+
+def random_forest_train_test(x_train, x_test, y_train, y_test):
+    # To-Do: Implement this function
+    rf_cls = RandomForestClassifier()
+    rf_cls.fit(x_train, y_train)
+    Acc = accuracy_score(y_test, rf_cls.predict(x_test))
+    mat = confusion_matrix(y_test, rf_cls.predict(x_test))
+    tp = mat[0][0]
+    fp = mat[0][1]
+    fn = mat[1][0]
+    Prec = tp/(tp+fp)
+    Rec = tp/(tp+fn)
+    return Acc, Prec, Rec
+
+
+def svm_train_test(x_train, x_test, y_train, y_test):
+    # To-Do: Implement this function
+    svm_pipe = make_pipeline(
+        StandardScaler(),
+        SVC()
+    )
+    svm_pipe.fit(x_train, y_train)
+    Acc = accuracy_score(y_test, svm_pipe.predict(x_test))
+    mat = confusion_matrix(y_test, svm_pipe.predict(x_test))
+    tp = mat[0][0]
+    fp = mat[0][1]
+    fn = mat[1][0]
+    Prec = tp/(tp+fp)
+    Rec = tp/(tp+fn)
+    return Acc, Prec, Rec
+
+
+def print_performances(acc, prec, recall):
+    # Do not modify this function!
+    print("Accuracy: ", acc)
+    print("Precision: ", prec)
+    print("Recall: ", recall)
+
+
+if __name__ == '__main__':
+    # Do not modify the main script!
+    data_path = sys.argv[1]
+    data_df = load_dataset(data_path)
+
+    n_feats, n_class0, n_class1 = dataset_stat(data_df)
+    print("Number of features: ", n_feats)
+    print("Number of class 0 data entries: ", n_class0)
+    print("Number of class 1 data entries: ", n_class1)
+
+    print("\nSplitting the dataset with the test size of ", float(sys.argv[2]))
+    x_train, x_test, y_train, y_test = split_dataset(
+        data_df, float(sys.argv[2]))
+
+    acc, prec, recall = decision_tree_train_test(
+        x_train, x_test, y_train, y_test)
+    print("\nDecision Tree Performances")
+    print_performances(acc, prec, recall)
+
+    acc, prec, recall = random_forest_train_test(
+        x_train, x_test, y_train, y_test)
+    print("\nRandom Forest Performances")
+    print_performances(acc, prec, recall)
+
+    acc, prec, recall = svm_train_test(x_train, x_test, y_train, y_test)
+    print("\nSVM Performances")
+    print_performances(acc, prec, recall)
